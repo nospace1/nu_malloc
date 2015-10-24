1c91b09239576d1b674ed0a055a1223b818d182d
Eric Darr <eric.m.darr@wmich.edu>
2015-10-23 22:28:26 -0400

    improved malloc, and implemented free and calloc

--- nu_malloc.c
+++ nu_malloc.c
@@ -1,19 +1,19 @@
 #include <stdlib.h>
 #include <unistd.h>
-#include <stdio.h>
+#include <stdio.h> 
+#include <string.h>
 
 #define PREDEFAMT 1024   //default amount of bytes the user gets whening using nu_malloc
 #define STRUCTSIZE 16    //the default byte size of the struct/header
 #define NUL '\0'
 
-typedef enum 
-{ false, true } boolean;
+typedef enum { false, true } boolean; 
 
-typedef struct _heap_block heap_block;
-struct _heap_block {
+typedef struct _heap_block heap_block; 
+struct _heap_block { 
     int size;   
-    boolean free;
-    heap_block *next; //THIS IS 8 BYTES ON 64-bit
+    boolean free; 
+    heap_block *next; //THIS IS 8 BYTES ON 64-bit 
 };
 
 heap_block *head = NUL;
@@ -21,31 +21,56 @@ heap_block *head = NUL;
 int main(void)
 {
     void *nu_malloc(size_t);
-    
-    size_t testsize = 1200;
-    size_t testsize2 = 300;
-    size_t testsize3 = 32;
+    void *nu_calloc(size_t, size_t);
+    void nu_free(void *);
+    void *nu_realloc(void *, size_t);
+
+    size_t testsize = 8000;
+    size_t testsize2 = 500;
+    size_t testsize3 = 2048;
+    size_t testsize4 = 316;
+    size_t testsize5 = 500;
+    size_t ctestsize4 = 500;
  
-    void *testPtr = nu_malloc(testsize);
     char *letters;
     char *letters2;
     char *letters3;
+    char *letters4;
 
-    letters = (char *)testPtr;
+    heap_block *node;
+    node = head;
+
+    void *testPtr = nu_calloc(30,ctestsize4);
+    void *testPtr6 = nu_malloc(testsize);
     void *testPtr2 = nu_malloc(testsize2);
-    letters2 = (char *)testPtr2;
     void *testPtr3 = nu_malloc(testsize3);
-    letters3 = (char *) testPtr3;
- 
-    int i = 0;
-    for(i = 0; i < testsize; i++)
+    void *testPtr4 = nu_malloc(testsize4);
+    void *testPtr5 = nu_malloc(testsize5);
+
+    node = head;
+    printf("----------------\n");
+     while(node != NUL)
     {
-        *(letters+i) = 'A'; 
-        if(i < testsize2)
-            *(letters2+i) = 'B'; 
-        if(i < testsize3)
-            *(letters3+i) = 'C'; 
+        printf("Node starts at %p. Free: %d. Size: %d. Next: %p\n", node, node->free, node->size, node->next);
+        node = node->next;
     }
+    
+
+    nu_free(testPtr6);
+    nu_free(testPtr5);
+    nu_free(testPtr4);
+    nu_free(testPtr3);
+    nu_free(testPtr2);
+    nu_free(testPtr);
+ 
+    node = head;
+    printf("----------------\n");
+     while(node != NUL)
+    {
+        printf("Node starts at %p. Free: %d. Size: %d. Next: %p\n", node, node->free, node->size, node->next);
+        node = node->next;
+    }
+
     return 0;
 }
 
@@ -70,13 +95,13 @@ void* nu_malloc(size_t size)
             n = ((size/PREDEFAMT)+1)*PREDEFAMT;
             head = sbrk(n+STRUCTSIZE);
             head->next = (heap_block *)(((char *)head)+(STRUCTSIZE+size));
-            head->next->size = n-size; 
+            head->next->size = n-(size+STRUCTSIZE); 
         }
         else
         {
-            head = sbrk(size+STRUCTSIZE);        
+            head = sbrk(PREDEFAMT+STRUCTSIZE);        
             head->next = (heap_block *)(((char *)head)+(STRUCTSIZE+size));
-            head->next->size = PREDEFAMT - size;
+            head->next->size = PREDEFAMT - (size+STRUCTSIZE);
         }
         //cut into two pieces
         head->size = size;
@@ -88,7 +113,7 @@ void* nu_malloc(size_t size)
         ptr = (void *)(((char *)head)+STRUCTSIZE);
         return ptr;
     }
-    else //head is not null
+    else //head is not null 
     {
         //we must search the list
         //create new node for searching
@@ -96,7 +121,7 @@ void* nu_malloc(size_t size)
         node = head;
         
         //start traversial
-        while((node->next != NUL) || (node->free == true)) //if next node is null, then the current node is the last node
+        while((node->next != NUL)) //if next node is null, then the current node is the last node
         {
             if(node->free == false)
             {
@@ -105,18 +130,19 @@ void* nu_malloc(size_t size)
             }
             else if(node->free == true)
             {
-               if(node->size == size)
+                if(node->size == size)
                 {
+                    node->free = false;
                     ptr = (void *)(((char *)node)+STRUCTSIZE);
                     return ptr;
                 } 
-                else if(node->size < (size+STRUCTSIZE)) // the node doesn't have enough space for both memory and following struct
+                else if((node->size < (size+STRUCTSIZE))) // && (node->next != NUL)) // the node doesn't have enough space for both memory and following struct
                 {
                     node = node->next; //progress to next node
                 }
                 else if(node->size > (size+STRUCTSIZE)) //if there is space for the struct and memory
                 {
-                    //insert a new node
+
                     heap_block *tempNode;
                     tempNode = (heap_block *)((char *)node + (STRUCTSIZE+size));
                     tempNode->next = node->next;
@@ -130,32 +156,157 @@ void* nu_malloc(size_t size)
                 }
             }
         }
-        //If I got to this point there was no free node that could hold the data
-        //currently at last node
-        //attach to end of list
-        if(size >= PREDEFAMT)
+        //I'm at the last node, check to see if it's free
+        if(node->free == true)
         {
-            //sbrk multiple of predefamt until greater than
-            int n = 0;
-            n = ((size/PREDEFAMT)+1)*PREDEFAMT;
-            node->next  = sbrk(n+STRUCTSIZE);
-            node->next->next = (heap_block *)(((char *)head)+(STRUCTSIZE+size));
-            node->next->next->size = n-size; 
+            if(node->size == size)
+            {
+                ptr = (void *)(((char *)node)+STRUCTSIZE);
+                return ptr;
+            } 
+            else if(node->size < (size+STRUCTSIZE)) //at last node, and it's not enough room
+            {
+                if(size >= PREDEFAMT)
+                {
+                    int n = 0;
+                    n = ((size/PREDEFAMT)+1)*PREDEFAMT;
+                    node->next = sbrk(n+STRUCTSIZE);
+                    node = node->next; // move foward, to last section/node so I can split it easier
+                    node->size = size;
+                    node->free = false;
+                    node->next = (heap_block *)(((char *)node)+(STRUCTSIZE+size)); 
+                    node->next->size = n - (size+STRUCTSIZE);
+                    node->next->free = true;
+                    ptr = (void *)(((char *)node)+STRUCTSIZE);
+                    node->next->next = NUL;
+                    return ptr;
+                }
+                else
+                {
+                    node->next = sbrk(PREDEFAMT+STRUCTSIZE);
+                    node = node->next;
+                    node->size = size;
+                    node->free = false;
+                    node->next = (heap_block *)(((char *)node)+(STRUCTSIZE+size));
+                    node->next->size = PREDEFAMT - (size+STRUCTSIZE);
+                    node->next->free = true;
+                    ptr = (void *)(((char *)node)+STRUCTSIZE);
+                    node->next->next = NUL;
+                    return ptr; 
+                } 
+            }
+            else if(node->size > (size+STRUCTSIZE)) //if there is space for the struct and memory
+            {
+                //insert a new node
+                heap_block *tempNode;
+                tempNode = (heap_block *)(((char *)node) + (STRUCTSIZE+size));
+                tempNode->next = node->next;
+                node->next = tempNode;
+                node->next->size = node->size - (int)(size+STRUCTSIZE);
+                node->size = size;
+                node->free = false;
+                node->next->free = true;
+                ptr = (void *)(((char *)node)+STRUCTSIZE);
+                return ptr;
+            }
         }
         else
         {
-            node->next = sbrk(size+STRUCTSIZE);        
-            node->next->next = (heap_block *)(((char *)head)+(STRUCTSIZE+size));
-            node->next->next->size = PREDEFAMT - size;
+            //If I got to this point there was no free node that could hold the data
+            //currently at last node
+            //attach to end of list
+            if(size >= PREDEFAMT)
+            {
+                //sbrk multiple of predefamt until greater than
+                int n = 0;
+                n = ((size/PREDEFAMT)+1)*PREDEFAMT;
+                node->next  = sbrk(n+STRUCTSIZE);
+                node->next->next = (heap_block *)(((char *)head)+(STRUCTSIZE+size));
+                node->next->next->size = n-size; 
+            }
+            else
+            {
+                node->next = sbrk(PREDEFAMT+STRUCTSIZE);        
+                node->next->next = (heap_block *)(((char *)head)+(STRUCTSIZE+size));
+                node->next->next->size = PREDEFAMT - size;
+            }        
+            // move to node to return, second to last node
+            node = node->next;
+            node->free = false;
+            node->next->free = true;
+
+            //the node after last node should be NULL
+            node->next->next = NUL;
+            ptr = (void *)(((char *)node)+STRUCTSIZE);
+            return ptr;
         }
-        // move to node to return, second to last node
-        node = node->next;
-        //the node after last node should be NULL
-        node->next->next = NUL;
-        ptr = (void *)(((char *)node)+STRUCTSIZE);
     }
 }
 
+void* nu_calloc(size_t count, size_t size)
+{
+    void *ptr = nu_malloc(size);
+    memset(ptr, 0, count);
+    return ptr;
+}
 
+void nu_free(void *ptr)
+{
+    heap_block *node;
+    node = head;
+
+    heap_block *prevNode = NUL;
+
+    //find node user specified
+    while(node != (heap_block*)(((char *)ptr)-STRUCTSIZE)) 
+    {
+        prevNode = node;
+        node = node->next;
+    }
+    if(head != NUL)
+    {    
+        node->free = true;
+    } 
+    //if here we know we are somewhere in the middle
+    if(prevNode != NUL && node->next != NUL)
+    {
+        if(prevNode->free == true && node->next->free == true)
+        {
+            prevNode->size = prevNode->size + STRUCTSIZE + node->size + STRUCTSIZE + node->next->size;
+            prevNode->next = node->next->next;
+        }
+        else if(prevNode->free == true)
+        {
+            prevNode->size = prevNode->size + node->size + STRUCTSIZE;
+            prevNode->next = node->next;
+        }
+        else if(node->next->free == true)
+        {
+            node->size = node->size + STRUCTSIZE + node->next->size;
+            node->next = node->next->next;
+        }
+    }
+    //check to see if there is null in prev and next
+    //  if prev is null then we are at head node, if next is null then we are at end.
+    if(prevNode == NUL)
+    {
+        if(node->next->free == true)
+        {
+            node->size = node->size + STRUCTSIZE + node->next->size;
+            node->next = node->next->next;
+        }  
+
+    }
+    // if here, we are at last node, do only checks for last node
+    else if(node->next == NUL)
+    {
+        if(prevNode->free == true)
+        {
+            //prevNode->size = (int)(((char *)node->next - (char *)prevNode)-STRUCTSIZE);
+            prevNode->size = prevNode->size + node->size + STRUCTSIZE;
+            prevNode->next = node->next;
+        }
+    }
+}
 
 
